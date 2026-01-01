d5bdf91d416af545f80a910ce64f57700bf84ae4
MattXLei <49255967+MattXLei@users.noreply.github.com>
2026-01-01 15:01:54 -0500

    Initial Commit

---
+++ MapTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapTracker
+//
+//  Created by InglouriousBasterd on 1/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapTracker/MapTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTrackerApp.swift
+//  MapTracker
+//
+//  Created by InglouriousBasterd on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MapTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
